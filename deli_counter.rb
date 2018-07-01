7485735075027b2a129332a265e33ba109a83e08
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-01 22:58:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,11 @@ def line(array)
     puts "The line is currently empty."
     return
   end
-  new_array = []
+  
   new_array = array.collect!.with_index{ |x, i| " #{i+1}. #{x}"}
 
   puts "The line is currently:#{new_array.join}"
   
 end
+
+def take a number
