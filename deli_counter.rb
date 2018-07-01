ca83c75b5fd9ecaa60c9c9664d795240e29d2588
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-01 22:57:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  
+  if array.length == 0
+    puts "The line is currently empty."
+    return
+  end
+  new_array = []
+  new_array = array.collect!.with_index{ |x, i| " #{i+1}. #{x}"}
+
+  puts "The line is currently:#{new_array.join}"
+  
+end
